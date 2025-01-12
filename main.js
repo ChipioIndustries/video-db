fbd0035767838f81213f5766358979ba54ad81d4
ChipioIndustries <53354220+ChipioIndustries@users.noreply.github.com>
2025-01-12 15:28:21 -0800

    progress

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-var db = fetch('db.json').then(response => response.json()).catch(err => console.error(err))
-console.log(db)
+var db = await fetch('db.json').then(response => response.json()).catch(err => console.error(err))
+console.log(db.data[0].title)
