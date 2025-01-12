68babc8965315443b31083f50c2460a01829dcf1
ChipioIndustries <53354220+ChipioIndustries@users.noreply.github.com>
2025-01-12 15:52:04 -0800

    bleh

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-var db = await fetch('db.json').then(response => response.json()).catch(err => console.error(err))
-console.log(db.data[0].title)
+let data = {};
+
+function update_visual() {
+	document.getElementsByName("media-title").forEach((value, key, parent) => {
+		value.remove();
+	})
+	data.forEach((value, index, array) => {
+		document.getElementById("insert-here").insertAdjacentHTML("afterend", "<p name='media-title'>" + value.title + "</p>")
+	})
+}
+
+async function handleDb() {
+	await fetch('db.json').then(response => response.json()).then(response => { data = response.data }).catch(err => console.error(err))
+	update_visual();
+}
+
+handleDb();
