6dadf96455b283d8f844154f1cd18f36e59407a5
ChipioIndustries <53354220+ChipioIndustries@users.noreply.github.com>
2025-01-13 18:16:49 -0800

    Polish

--- main.js
+++ main.js
@@ -1,36 +1,10 @@
 let data = [];
-let tab = "video";
-let contentType = "movie";
+let mediaTypes = {};
+let videoTypes = {};
 let searchTerm = "";
 let selection = null;
 
-function selectTab(tabName) {
-	tab = tabName;
-	document.querySelectorAll(".tab").forEach((value, index, array) => {
-		if (value.getAttribute("name") == tabName) {
-			value.classList.add("selected");
-		} else {
-			value.classList.remove("selected");
-		}
-	});
-	document.querySelectorAll(".tab-page").forEach((value, index, array) => {
-		if (value.getAttribute("name") == tabName) {
-			value.classList.remove("hidden")
-		} else {
-			value.classList.add("hidden")
-		}
-	})
-}
-
-function detectTabClicks() {
-	document.querySelectorAll(".tab").forEach((value, index, array) => {
-		value.onclick = function () {
-			selectTab(value.name);
-		}
-	})
-}
-
-function setFilters() {
+function setFilterFromURL() {
 	const url = new URL(window.location.toString()).searchParams;
 	if (url.get("selection")) {
 		document.querySelector("#filter").value = url.get("selection");
@@ -42,26 +16,56 @@ function updateResults() {
 		value.remove();
 	})
 	getFilteredData().forEach((value, index, array) => {
-		document.querySelector("#video-tab-page").insertAdjacentHTML("beforeend", makeEntryDisplay(value))
+		document.querySelector(".entries").insertAdjacentHTML("beforeend", makeEntryDisplay(value))
 	})
 }
 
+function formatSeconds(sec_num) {
+	var hours = Math.floor(sec_num / 3600);
+	var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+	var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+	if (minutes < 10) { minutes = "0" + minutes; }
+	if (seconds < 10) { seconds = "0" + seconds; }
+	return hours + ':' + minutes + ':' + seconds;
+}
+
 function makeEntryDisplay(entry) {
-	return `
+	let year = entry.year.toString()
+	if (entry["year-end"]) {
+		year = entry.year.toString() + ` - ` + entry["year-end"]
+	}
+	let display = `
 	<div id="entry-`+ entry.name + `" class="entry" name="` + entry.name + `">
 		<img src="covers/`+ entry.name + `.png" class="cover">
 		<div class="entry-details">
-			<h2 class="entry-title">`+ entry.title + `</h2>
-			<p><b>UPC:</b> `+ entry.upc + `</p>
+			<div class="entry-title-div"><h2 class="entry-title">`+ entry.title + ` (` + year + `)</h2> <h3 class="creator-name">by ` + entry.creator + `</h3></div>
+			<p class="entry-detail"><b>Media Type:</b> `+ entry["media-type"] + `</p>`
+	if (entry["video-type"]) {
+		display = display + `<p class="entry-detail"><b>Video Type:</b> ` + entry["video-type"] + `</p>`
+	}
+	if (entry["media-format"]) {
+		display = display + `<p class="entry-detail"><b>Media Format:</b> ` + entry["media-format"] + `</p>`
+	}
+	if (entry.runtime) {
+		display = display + `<p class="entry-detail"><b>Runtime:</b> ` + formatSeconds(entry.runtime) + `</p>`
+	}
+	display = display + `
+			<p class="entry-detail"><b>UPC:</b> ` + entry.upc + `</p>
 			<p class="description">`+ entry.description + `</p>
+			<div>
+				<a href="details?name=` + entry.name + `" class="details"><b>Details \></b></a>
+			</div>
 		</div>
 	</div>
 `
+
+	return display
 }
 
 function getFilteredData() {
 	return data.filter((value, index, array) => {
-		if (value["media-type"] == tab && value["content-type"] == contentType && value.title.toLowerCase().search(searchTerm.toLowerCase()) != -1) {
+		if (mediaTypes[value["media-type"]] && videoTypes[value["video-type"]] && value.title.toLowerCase().search(searchTerm.toLowerCase()) != -1) {
 			return true
 		} else {
 			return false
@@ -74,15 +78,30 @@ function getEntryByName(name) {
 }
 
 async function loadDb() {
-	await fetch('db.json').then(response => response.json()).then(response => { data = response.data }).catch(err => console.error(err))
+	await fetch('db.json')
+		.then(response => response.json())
+		.then(response => { data = response.data })
+		.catch(err => console.error(err))
 	updateResults();
 }
 
 function handleFilters() {
-	document.querySelector("#media-type").addEventListener("change", () => {
-		contentType = document.querySelector("#media-type").value;
-		updateResults();
+	document.querySelectorAll(".media-type-input").forEach((value, key, parent) => {
+		mediaTypes[value.getAttribute("value")] = value.checked;
+		value.addEventListener("change", () => {
+			mediaTypes[value.getAttribute("value")] = value.checked;
+			updateResults();
+		})
 	})
+
+	document.querySelectorAll(".video-type-input").forEach((value, key, parent) => {
+		videoTypes[value.getAttribute("value")] = value.checked;
+		value.addEventListener("change", () => {
+			videoTypes[value.getAttribute("value")] = value.checked;
+			updateResults();
+		})
+	})
+
 	document.querySelector("#filter").addEventListener("input", () => {
 		searchTerm = document.querySelector("#filter").value;
 		updateResults();
@@ -90,9 +109,7 @@ function handleFilters() {
 }
 
 window.onload = async function () {
-	selectTab("video");
-	setFilters();
+	setFilterFromURL();
 	handleFilters();
-	detectTabClicks();
 	await loadDb();
 }
